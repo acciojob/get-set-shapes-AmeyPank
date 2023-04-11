6b09563980b53044dc4d69329d0559c62466e9be
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-11 23:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
+// //complete this code
+// // Rectangle class
+// class Rectangle {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   get width() {
+//     return this.width;
+//   }
+
+//   get height() {
+//     return this.height;
+//   }
+
+//   getArea() {
+//     return this.width * this.height;
+//   }
+// }
+
+// // Square class
+// class Square extends Rectangle {
+//   constructor(side) {
+//     super(side, side);
+//     this.side = side;
+//   }
+
+//   getPerimeter() {
+//     return 4 * this.side;
+//   }
+// }
+
+// // Do not change the code below this line
+// window.Rectangle = Rectangle;
+// window.Square = Square;
+
+// // Do not change the code below this line
+// window.Rectangle = Rectangle;
+// window.Square = Square;
+
+
 //complete this code
 // Rectangle class
 class Rectangle {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+    this._width = width;
+    this._height = height;
   }
 
   get width() {
-    return this.width;
+    return this._width;
   }
 
   get height() {
-    return this.height;
+    return this._height;
   }
 
   getArea() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 }
 
@@ -23,11 +65,11 @@ class Rectangle {
 class Square extends Rectangle {
   constructor(side) {
     super(side, side);
-    this.side = side;
+    this._side = side;
   }
 
   getPerimeter() {
-    return 4 * this.side;
+    return 4 * this._side;
   }
 }
 
